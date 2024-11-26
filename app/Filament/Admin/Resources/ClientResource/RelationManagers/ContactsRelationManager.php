44be1df6584f607086b1c8dc34ee31babd24d99f
Whizzy <hey@whizzy.dev>
2024-11-26 19:08:30 +0000

    Create ClientResource in admin panel

---
+++ app/Filament/Admin/Resources/ClientResource/RelationManagers/ContactsRelationManager.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Filament\Admin\Resources\ClientResource\RelationManagers;
+
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Infolists;
+use Filament\Infolists\Infolist;
+use Filament\Resources\RelationManagers\RelationManager;
+use Filament\Tables;
+use Filament\Tables\Table;
+
+class ContactsRelationManager extends RelationManager
+{
+    protected static string $relationship = 'contacts';
+
+    public function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('email')
+                    ->email()
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\TextInput::make('phone')
+                    ->maxLength(255),
+            ]);
+    }
+
+    public function infolist(Infolist $infolist): Infolist
+    {
+        return $infolist
+            ->schema([
+                Infolists\Components\TextEntry::make('email'),
+                Infolists\Components\TextEntry::make('phone'),
+                Infolists\Components\TextEntry::make('created_at')
+                    ->dateTime(),
+                Infolists\Components\TextEntry::make('updated_at')
+                    ->dateTime(),
+            ]);
+    }
+
+    public function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('email')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('phone')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+            ])
+            ->headerActions([
+                Tables\Actions\CreateAction::make(),
+                Tables\Actions\AssociateAction::make(),
+            ])
+            ->actions([
+                Tables\Actions\ViewAction::make(),
+                Tables\Actions\EditAction::make(),
+                Tables\Actions\DissociateAction::make(),
+                Tables\Actions\DeleteAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DissociateBulkAction::make(),
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ])
+            ->recordTitleAttribute('email');
+    }
+}
---
+++ app/Filament/Admin/Resources/ClientResource/RelationManagers/ProjectsRelationManager.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace App\Filament\Admin\Resources\ClientResource\RelationManagers;
+
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Infolists;
+use Filament\Infolists\Infolist;
+use Filament\Resources\RelationManagers\RelationManager;
+use Filament\Tables;
+use Filament\Tables\Table;
+
+class ProjectsRelationManager extends RelationManager
+{
+    protected static string $relationship = 'projects';
+
+    public function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('name')
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\MarkdownEditor::make('description')
+                    ->maxLength(65535)
+                    ->columnSpan('full'),
+                Forms\Components\TextInput::make('price')
+                    ->numeric()
+                    ->required(),
+                Forms\Components\TextInput::make('hourly_rate')
+                    ->numeric()
+                    ->required(),
+                Forms\Components\TextInput::make('total_hours_spent')
+                    ->numeric()
+                    ->required(),
+            ]);
+    }
+
+    public function infolist(Infolist $infolist): Infolist
+    {
+        return $infolist
+            ->schema([
+                Infolists\Components\TextEntry::make('name'),
+                Infolists\Components\TextEntry::make('description')
+                    ->markdown()
+                    ->columnSpan('full'),
+                Infolists\Components\TextEntry::make('price')
+                    ->numeric(),
+                Infolists\Components\TextEntry::make('hourly_rate')
+                    ->numeric(),
+                Infolists\Components\TextEntry::make('total_hours_spent')
+                    ->numeric(),
+                Infolists\Components\TextEntry::make('created_at')
+                    ->dateTime(),
+                Infolists\Components\TextEntry::make('updated_at')
+                    ->dateTime(),
+            ]);
+    }
+
+    public function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('name')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('price')
+                    ->numeric()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('hourly_rate')
+                    ->numeric()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('total_hours_spent')
+                    ->numeric()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+            ])
+            ->headerActions([
+                Tables\Actions\CreateAction::make(),
+                Tables\Actions\AssociateAction::make(),
+            ])
+            ->actions([
+                Tables\Actions\ViewAction::make(),
+                Tables\Actions\EditAction::make(),
+                Tables\Actions\DissociateAction::make(),
+                Tables\Actions\DeleteAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DissociateBulkAction::make(),
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ])
+            ->recordTitleAttribute('name');
+    }
+}
